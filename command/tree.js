7b14de3df6e55e5ad0e06562b273a47f16a4d44b
shekhar2702 <shekhar.prince1234@gmail.com>
2021-07-25 22:10:30 +0530

    Added recursive solution to print sub-folders

--- command/tree.js
+++ command/tree.js
@@ -1,15 +1,37 @@
 let path = require("path");
 let fs = require("fs");
-let tree = function (pathFile) {
+let tree = function (pathFile, level) {
   let folderName = path.basename(pathFile);
   console.log(folderName);
-  console.log("\t|");
-  console.log("\t|");
+  for (let i = 1; i <= level; i++) console.log("\t|");
+
   let folderContents = fs.readdirSync(pathFile);
   for (let i = 0; i < folderContents.length; i++) {
-    console.log("\t|----------------" + folderContents[i]);
+    let statOfFiles = fs.lstatSync(path.join(pathFile, folderContents[i]));
+    if (statOfFiles.isFile()) {
+      console.log();
+      for (let j = 1; j <= level; j++) {
+        process.stdout.write("\t");
+      }
+      process.stdout.write(`|-------${folderContents[i]}`);
+    } else {
+      let currentDirPath = path.join(pathFile, folderContents[i]);
+      tree(currentDirPath);
+    }
   }
 };
 module.exports = {
   tree,
 };
+// level1
+//     |
+//     |----
+//     |----
+//     |
+//     level2
+//         |----
+//         |----
+//         level3
+//             |----
+//             |----
+//         |----
--- main.js
+++ main.js
@@ -3,18 +3,18 @@ let tree = require("./command/tree");
 let organize = require("./command/organize");
 let fs = require("fs");
 let inputArr = process.argv;
-let cmdExecute = inputArr[2]
-let pathFile = inputArr[3]
-switch(cmdExecute){
-    case "help":
-        help.help()
-        break
-    case "organize":
-        organize.organize(pathFile)
-        break
-    case "tree":
-        tree.tree(pathFile)
-        break
-    default:
-        console.log("Wrong Input")
+let cmdExecute = inputArr[2];
+let pathFile = inputArr[3];
+switch (cmdExecute) {
+  case "help":
+    help.help();
+    break;
+  case "organize":
+    organize.organize(pathFile);
+    break;
+  case "tree":
+    tree.tree(pathFile, 1);
+    break;
+  default:
+    console.log("Wrong Input");
 }
